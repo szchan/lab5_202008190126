aa21e620530a1b5390ea9b1efd54d51b9d780ce5
Shaoze Chan <mail.szchan@gmail.com>
2022-11-11 08:22:29 +0800

    inite

---
+++ app/src/main/java/com/szchan/lab5_202008190126/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.szchan.lab5_202008190126
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
